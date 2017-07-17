f4acbaa949050467483c8da9acf92c9649f20b82
Henrik Aarnio <hjaarnio@gmail.com>
2017-07-17 12:03:19 +0300

    add encrypted invite links

--- api/areas.js
+++ api/areas.js
@@ -1,23 +1,28 @@
 var db = require('../models/index.js');
 var express = require('express');
 var router = express.Router();
+var encrypter = require('../encrypter.js');
 
 router.post('/new', function(req, res, next) {
   console.dir(req.body)
   db.Area.create({
     name: req.body.name
   }).then((area) => {
-    return db.Permission.create({
-      AreaName: req.body.name,
-      user_id: req.session.passport.user._json.sub,
-      admin: true
-    })
-  }).then((permission) => {
+    return new Promise((resolve, reject) => {
+      db.Permission.create({
+        AreaName: req.body.name,
+        user_id: req.session.passport.user._json.sub,
+        admin: true
+      }).then(permission => resolve({permission, area}))
+    });
+  }).then((result) => {
     res.send({
       status:"200 area added",
       area: {
-        name: permission.dataValues.AreaName,
-        admin: true
+        name: result.area.dataValues.name,
+        admin: true,
+        inviteLink: encrypter.encrypt('user|'+result.area.dataValues.uuid),
+        adminInviteLink: encrypter.encrypt('admin|'+result.area.dataValues.uuid)
       }
     });
   });
@@ -25,12 +30,14 @@ router.post('/new', function(req, res, next) {
 
 router.get('/all', function(req, res, next) {
   console.dir(req.session.passport.user)
-  db.Permission.findAll({where: {user_id: req.session.passport.user._json.sub}}).then(permissions => {
+  db.Permission.findAll({where: {user_id: req.session.passport.user._json.sub}, include: db.Area}).then(permissions => {
     return permissions.map(permission => {
       console.log(permission)
       return {
         name: permission.dataValues.AreaName,
-        admin: permission.dataValues.admin
+        admin: permission.dataValues.admin,
+        inviteLink: encrypter.encrypt('user|'+permission.Area.dataValues.uuid),
+        adminInviteLink: encrypter.encrypt('admin|'+permission.Area.dataValues.uuid)
       };
     })
   }).then(areas => {
